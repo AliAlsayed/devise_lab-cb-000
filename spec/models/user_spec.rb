748cea30a5d11ef2e95bfc459a09899274b9ecd0
Ali Alsayed <a_alsayed87@hotmail.com>
2019-06-17 07:42:35 +0000

    Automatically backed up by Learn

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
   subject { @user }
 
   # it { should respond_to(:email) }
-  # 
+  #
   # it "#email returns a string" do
   #   expect(@user.email).to match 'user@example.com'
   # end
