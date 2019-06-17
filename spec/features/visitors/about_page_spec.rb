f0fd176acd661a0bb4602139033fcc2b43e36999
Ali Alsayed <a_alsayed87@hotmail.com>
2019-06-17 07:44:57 +0000

    Automatically backed up by Learn

--- spec/features/visitors/about_page_spec.rb
+++ spec/features/visitors/about_page_spec.rb
@@ -3,7 +3,7 @@
 # #   I want to visit an 'about' page
 # #   So I can learn more about the website
 # feature 'About page' do
-# 
+#
 #   # Scenario: Visit the 'about' page
 #   #   Given I am a visitor
 #   #   When I visit the 'about' page
